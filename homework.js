4b62ed3fc76dcf32960ce92f27776cdb81dd5885
Suleyman <hhhh@fsdfdsf.mdf>
2021-11-24 01:56:39 +0400

    Homework done

--- homework.js
+++ homework.js
@@ -1,5 +1,23 @@
 "use strict" ;
 
-let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?" , "") ;
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?" , "") ;
 
-console.log(numberOfFilms) ;
+const personalMovideDB = {
+             count: numberOfFilms ,
+             movies: {},
+             actors: {},
+             genres: [],
+             privat:false 
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', '') ,
+      b = prompt('Оцените фильм','') ,
+      c = prompt('Один из последних просмотренных фильмов?', '') ,
+      d = prompt('Оцените фильм' , '') ;
+
+personalMovideDB.movies[a] = b ;
+personalMovideDB.movies[c] = d ;
+
+console.log(personalMovideDB) ;
+
+
